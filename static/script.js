3ccbed9bd40bde2c741cac7596642d0a60aae475
rozeroze <rosettastone1886@gmail.com>
2021-02-15 00:07:58 +0900

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,4 +1,7 @@
 (function () {
+  // top-app-bar
+  const topAppBar = new MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
+
   // text-field
   const textFields = document.querySelectorAll('.mdc-text-field');
   for (const textField of textFields) {
