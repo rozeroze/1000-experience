62d19bfc0b824d4f7e8bbe115842e827852ba465
rozeroze <rosettastone1886@gmail.com>
2021-03-28 18:42:18 +0900

    Update script.js

--- static/script.js
+++ static/script.js
@@ -2,7 +2,7 @@
 const INITIAL_STARS = "star-3";
 const INITIAL_CURRENT_LEVEL = 1;
 const INITIAL_TARGET_LEVEL = 99;
-const FLG_LIMIT_LEVEL_CORRECTION = true;
+const FLG_LIMIT_VALUE_CORRECTION = true;
 const MAX_LEVELS = {
   "star-1": 30,
   "star-2": 40,
@@ -46,11 +46,52 @@ var get_total_exp_to_target;
   current_level = document.querySelector("[name=current-level]");
   target_level = document.querySelector("[name=target-level]");
   at_next = document.querySelector("[name=at-next]");
-  
+
   // plane-text
   necessary_experience = document.querySelector("div.necessary-experience-count");
-  
-  // get-value
+
+  // getter-setter
+  get_current_level = () => {
+    return current_level.value;
+  };
+  set_current_level = lv => {
+    let _level = lv;
+    let _max = get_max_level();
+    if (FLG_LIMIT_VALUE_CORRECTION) {
+      if (_max < _level) {
+        _level = _max;
+      }
+    }
+    current_level.value = _level;
+  };
+  get_target_level = () => {
+    return target_level.value;
+  };
+  set_target_level = lv => {
+    let _level = lv;
+    let _max = get_max_level();
+    if (FLG_LIMIT_VALUE_CORRECTION) {
+      if (_max < _level) {
+        _level = _max;
+      }
+    }
+    target_level.value = _level;
+  };
+  get_at_next = () => {
+    return at_next.value;
+  };
+  set_at_next = num => {
+    let _num = num;
+    let _max = get_at_next_max();
+    if (FLG_LIMIT_VALUE_CORRECTION) {
+      if (_max < _num) {
+        _num = _max;
+      }
+    }
+    at_next.value = _num;
+  };
+
+  // functions
   get_stars = () => document.querySelector("[name=stars]:checked").id;
   get_max_level = () => {
     return MAX_LEVELS[get_stars()];
@@ -65,28 +106,8 @@ var get_total_exp_to_target;
     return exp[_star][target_level.value] - exp[_star][current_level.value];
   };
 
-  // get-set
-  get_current_level = () => {
-    return current_level.value;
-  };
-  set_current_level = lv => {
-    current_level.value = lv;
-  };
-  get_target_level = () => {
-    return target_level.value;
-  };
-  set_target_level = lv => {
-    target_level.value = lv;
-  };
-  get_at_next = () => {
-    return at_next.value;
-  };
-  set_at_next = num => {
-    at_next.value = num;
-  };
-
   // initial-setting
   document.querySelector("[name=stars]#" + INITIAL_STARS).click();
-  current_level.value = INITIAL_CURRENT_LEVEL;
-  target_level.value = INITIAL_TARGET_LEVEL;
+  set_current_level(INITIAL_CURRENT_LEVEL);
+  set_target_level(INITIAL_TARGET_LEVEL);
 })()
