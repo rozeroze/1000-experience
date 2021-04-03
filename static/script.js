e4931c2a45aa5795d92ec8f2a49c296db4e7f4f8
rozeroze <rosettastone1886@gmail.com>
2021-04-03 21:26:32 +0900

    Update script.js

--- static/script.js
+++ static/script.js
@@ -65,9 +65,9 @@ var set_necessary_experience;
     //elem.click();
     star_holder.value = star;
     document.querySelectorAll("section.button .button-container button").forEach(e => {
-      e.classList.remove("test-bg-thin");
+      e.classList.add("test-bg-thin");
     });
-    document.querySelector("section.button .button-container button[data-stars='" + star + "']").classList.add("test-bg-thin");
+    document.querySelector("section.button .button-container button[data-stars='" + star + "']").classList.remove("test-bg-thin");
   };
   get_current_level = () => {
     return parseInt(current_level.value);
